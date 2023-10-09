ae137d29de5a6b2dd98855a535a1e127eeb958dd
mathiasburger <mburakyilmazer38@gmail.com>
2023-10-10 01:32:00 +0300

    reserved words

---
+++ teamwork-3.js
@@ -0,0 +1,38 @@
+// Write a JavaScript function called reverseWordsWithoutArray that takes a sentence as input and returns
+// the sentence with its words reversed, without using arrays:
+// This JavaScript code challenge involves reversing the words in a given sentence without using arrays. It's a
+// practical exercise that combines string manipulation, loops, and functions.
+// Expected Outputs :
+// For example, if the input is Hello Js World 2023, the function should return "2023 World Js Hello".
+
+let j;
+let reversedWord = "";
+
+function reverseWordsWithoutArray(word) {
+  for (let i = String(word).length - 1; i >= 0; i--) {
+    j = word[i];
+    reversedWord += j;
+  }
+  return console.log(reversedWord);
+}
+
+reverseWordsWithoutArray("Hello Js World 2023");
+
+// console.log("Hello Js World 2023".split(" ").length);
+
+// let john = "Hello Js World 2023".split(" ");
+// console.log(john[2]);
+
+let k;
+let reverse = " ";
+
+function reverseSentence(word) {
+  for (let i = word.split(" ").length - 1; i >= 0; i--) {
+    k = word.split(" ")[i];
+    // console.log(k)
+    reverse += k + " ";
+    // console.log(reverse)
+  }
+  return console.log(reverse);
+}
+reverseSentence("Hello Js World 2023");
