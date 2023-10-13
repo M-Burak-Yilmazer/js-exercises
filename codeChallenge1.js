74ca9e4a9c9a5bbd5ff18fa964663da0449c649f
mathiasburger <mburakyilmazer38@gmail.com>
2023-10-13 18:46:41 +0300

    code challenge 1

---
+++ codeChallenge1.js
@@ -0,0 +1,24 @@
+console.log("merhaba");
+// 1- 18 yaşından büyüklerin oy kullanabileceğini 18 yaşından küçüklerin oy kullanamayacağını if/else ve ternary yapı ile konsola yazdıran kodu yazınız.
+
+// 2- Celsius değerini Fahrenheit değerine dönüştüren bir fonksiyon yazınız? (Celsius × 9/5) + 32 = Fahrenheit
+
+// 3- Belirli bir string ifadenin tersini bulan fonksiyonu yazınız?
+
+// 4- Belirli bir string ifadenin palindrome olup olmadığını bulan fonksiyonu yazınız?
+
+// 5- Belirli bir string ifadenin içindeki sesli harfleri bulan fonksiyonu yazınız?
+
+// 6- Belirli bir sayının tam bölenlerinin toplamını bulan fonksiyonu yazınız?
+
+// 7- Belirli bir sayının asal olup olmadığını bulan fonksiyonu yazınız? 2, 3, 5, 7, 11, … şeklinde devam eden, kendisinden ve 1'den başka pozitif böleni olmayan 2 ve 2'den büyük sayılara “asal sayı” denir.
+
+// 8-Girilen bir cümledeki kelime sayısını bulan bir fonksiyon yazınız.
+
+// 9- Bir kelime ve bir harf olmak üzere İki adet string parametre alan ve girilen harften girilen kelime içinde kaç adet olduğunu sonuç olarak dönen bir fonksiyon yazınız.
+// Girilen harf kelime içinde yoksa 0 dönmeli.
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// Notes:
+// The first argument can be an empty string
